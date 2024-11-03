662ff99b62cc6c82065a1b7a7927d523f6adbec9
manar ahmed <manarahmedfahmey01@gmail.com>
2024-11-03 19:41:47 +0200

    Add swap.c file

---
+++ unit_2/lesson_3_C_basics/swap.c
@@ -0,0 +1,23 @@
+/*
+ * main.c
+ *
+ *  Created on: Oct 30, 2024
+ *      Author: Manar Ahmed
+ */
+
+
+#include"stdio.h"
+
+void main(){
+	float a ,b;
+	printf("Enter value of a: ");
+	fflush(stdout);
+	scanf("%f",&a);
+	printf("Enter value of b: ");
+		fflush(stdout);
+		scanf("%f",&b);
+
+	printf("After swapping, value of a %f\n",b);
+	printf("After swapping, value of b %f\n",a);
+}
+
